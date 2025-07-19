5f663a65ebb0ab031e33ac42e169ebd791b6d527
JJ-Cronos <jj@steed.co.za>
2025-07-19 15:46:17 +0200

    init: Add React

---
+++ frontend/coin_trade_app/src/routes/__root.tsx
@@ -0,0 +1,13 @@
+import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import NavBar from "../components/Navigation/NavBar";
+
+export const Route = createRootRoute({
+    component: () => (
+        <>
+            <NavBar />
+            <Outlet />
+            <TanStackRouterDevtools />
+        </>
+    ),
+});
