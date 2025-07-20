e015c288801ef9ddaa5a84e200655f39c1076764
JJ-Cronos <jj@steed.co.za>
2025-07-20 20:34:11 +0200

    feat: Add rate limiting and CORS middleware to the app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,17 @@
 import express from 'express'
 import apiRouter from './api/v1/api.route.ts'
 import { notFound, expressErrHandler } from './middleware/errorHandler.middleware.ts'
+import { corsMiddleware } from './middleware/cors.middleware.ts'
+import { rateLimiter } from './middleware/rateLimit.middleware.ts'
+
 const app = express()
+
+
+//rate limiting
+app.use(rateLimiter)
+
+//cors
+app.use(corsMiddleware)
 
 // App routes
 app.use('/api/v1', apiRouter)
