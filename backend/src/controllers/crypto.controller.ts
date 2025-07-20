2fa2a68128ae95b234471ac8f4ad55ee73cc6ccf
JJ-Cronos <jj@steed.co.za>
2025-07-20 20:34:32 +0200

    feat: Enhance Coin creation with additional attributes in getCryptos function

--- backend/src/controllers/crypto.controller.ts
+++ backend/src/controllers/crypto.controller.ts
@@ -1,6 +1,6 @@
 import type { Request, Response, NextFunction } from 'express';
 import { getCoins } from '../services/crypto.service.ts';
-import { Coin } from '../models/Coin.ts';
+import { Coin, type CoinCreationAttributes } from '../models/Coin.ts';
 import { redisClient } from '../database/redis.database.ts';
 import { hash } from 'crypto';
 
@@ -27,13 +27,17 @@ export const getCryptos = async (req: Request, res: Response, next: NextFunction
 
         //Store fresh data in database
         for (const item of data) {
-            const snapshotValues = {
+            const snapshotValues: CoinCreationAttributes = {
+                nameIdentifier: item.id,
                 name: item.name,
                 symbol: item.symbol,
                 image: item.image,
                 currentPrice: item.current_price * 100, //Convert floating point currency into cents
                 marketCap: item.market_cap * 100, //Convert floating point currency into cents
-                marketCapRank: item.market_cap_rank
+                marketCapRank: item.market_cap_rank,
+                priceChangePercentage24h: item.price_change_percentage_24h,
+                high24h: item.high_24h,
+                low24h: item.low_24h,
             }
             coins.push(await Coin.createOrUpdate(snapshotValues))
         }
