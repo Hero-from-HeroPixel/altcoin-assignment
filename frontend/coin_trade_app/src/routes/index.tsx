39e920114a8198e84aa04a218ed3b368d06e1d62
JJ-Cronos <jj@steed.co.za>
2025-07-20 20:30:15 +0200

    feat: Add basic home page layout

--- frontend/coin_trade_app/src/routes/index.tsx
+++ frontend/coin_trade_app/src/routes/index.tsx
@@ -1,13 +1,48 @@
 import { createFileRoute } from "@tanstack/react-router";
+import DataTable from "../components/table/DataTable";
+import { useGetCrypto } from "../hooks/useGetCrypto";
 
 export const Route = createFileRoute("/")({
     component: Index,
 });
 
 function Index() {
-    return (
-        <div className="p-2">
-            <h3>Welcome Home!</h3>
+    const { data } = useGetCrypto();
+    const cols = [
+        "Market Cap Rank",
+        "Logo",
+        "Coin Name",
+        "Current Price",
+        "High (24h)",
+        "Low (24h)",
+        "24h Change",
+    ];
+
+    const Fallback = (
+        <div className="w-full min-h-75 d-flex relative flex-column gap-1">
+            <div className="skeleton"></div>
+            <div className="skeleton"></div>
+            <div className="skeleton"></div>
+            <div className="skeleton"></div>
+            <div className="skeleton"></div>
+            <div className="skeleton"></div>
+            <div className="skeleton"></div>
+            <div className="skeleton"></div>
+            <div className="skeleton"></div>
+            <div className="skeleton"></div>
+            <div className="skeleton"></div>
+            <div className="skeleton"></div>
+            <p className="absolute absolute--center fs-2">Loading Coins...</p>
         </div>
+    );
+
+    return (
+        <section className="container">
+            <h1>Coin Trader</h1>
+            <p>View current crypto prices below.</p>
+            <div className="data-table scrollbar scrollbar--thin">
+                {data ? <DataTable cols={cols} data={data.data} /> : Fallback}
+            </div>
+        </section>
     );
 }
