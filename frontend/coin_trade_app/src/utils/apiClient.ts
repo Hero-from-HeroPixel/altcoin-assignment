63f4265316ef4527a2f26fa36d3d4af28ca81d20
JJ-Cronos <jj@steed.co.za>
2025-07-21 01:22:24 +0200

    fix: Change frontend api client timeout to 5s

--- frontend/coin_trade_app/src/utils/apiClient.ts
+++ frontend/coin_trade_app/src/utils/apiClient.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const apiClient = axios.create({
     baseURL: import.meta.env.VITE_BACKEND_URI,
-    timeout: 3000 //3 seconds
+    timeout: 5000 //5 seconds
 })
